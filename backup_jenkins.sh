f59a7da8c188a9c7ca2f179ed3921c415db8d81f
swnam <150226255+suwnam@users.noreply.github.com>
2025-03-17 17:48:27 +0900

    Update backup_jenkins.sh

--- backup_jenkins.sh
+++ backup_jenkins.sh
@@ -4,7 +4,7 @@
 ## version: v0.2
 ## date: 2025-03-17
 
-RESTIC_REPO_NEXUS=$RESTIC_REPO/test_jenkins
+RESTIC_REPO_JENKINS=$RESTIC_REPO/test_jenkins
 BACKUP_DIR="/home/swnam/Jenkins/jenkins_home"
 EXCLUDE_DIR=$BACKUP_DIR/workspace
 
