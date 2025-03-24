0c2c06d52ba2bdb3c03256b214c823e97eafa784
swnam <150226255+suwnam@users.noreply.github.com>
2025-03-24 15:36:41 +0900

    Update checklist_jenkins.sh

--- checklist_jenkins.sh
+++ checklist_jenkins.sh
@@ -4,19 +4,25 @@
 ## version: v0.1
 ## date: 2025-03-24
 
+RESTIC_REPO_JENKINS=$RESTIC_REPO/test_jenkins
+BACKUP_DIR="/home/popcornsar/DevOps/01_Jenkins/jenkins_home"
+
 # 스냅샷의 tag 가져오기
 BACKUP_DATE=$(date +%y%m%d)
-tags=$(restic -r "$RESTIC_REPO_JENKINS" snapshots latest --json | jq -r '.[-1].tags[]')
+
+snapshot_id=$(restic -r "$RESTIC_REPO_JENKINS" snapshots latest --json | jq -r '.[-1].short_id')
+tags=$(restic -r "$RESTIC_REPO_JENKINS" snapshots $snapshot_id --json | jq -r '.[-1].tags[]')
 
 # 오늘자 스냅샷 여부 확인
-if echo "$tags" | grep -q "^$BACKUP_DATE$"; then
-  echo "(+) found today snapshots."
+if [[ "$tags" == *"$BACKUP_DATE"* ]]; then
+  echo "[+] found today snapshots."
 else
-  echo "(-) No today snapshots found. exit..."
+  echo "[-] No today snapshots found. exit..."
   exit 1
 fi
 
-echo "(+) Target snapshot ID: $snapshot_id"
+
+echo "[*] Target snapshot ID: $snapshot_id"
 echo "==========================="
 
 # Primary Jenkins file lists
@@ -37,15 +43,15 @@ check_paths() {
   shift
   local paths=("$@")
 
-  echo "[+] Checking if exists $label..."
+  echo "[*] Checking if exists $label..."
   for path in "${paths[@]}"; do
     if restic -r $RESTIC_REPO_JENKINS ls "$snapshot_id" | grep -q "$path"; then
-      echo "(+) SUCCESS : $path"
+      echo "[+] SUCCESS : $path"
     else
-      echo "(-) FAIL : $path"
+      echo "[-] FAIL : $path"
     fi
   done
   echo
 }
 
-check_paths "Jenkins" "${jenkins_paths[@]}"
+check_paths "Jenkins_primary_files" "${jenkins_paths[@]}"
