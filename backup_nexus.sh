a45cb40040e434fbd1cb3f57cf0a2ffe84313a3c
swnam <150226255+suwnam@users.noreply.github.com>
2025-03-24 09:51:12 +0900

    Update backup_nexus.sh
    change $BACKUP_DIR

--- backup_nexus.sh
+++ backup_nexus.sh
@@ -5,7 +5,7 @@
 ## date: 2025-03-20
 
 RESTIC_REPO_NEXUS=$RESTIC_REPO/test_nexus
-BACKUP_DIR="/home/popcornsar/DevOps/03_Nexus/nexus-data"
+BACKUP_DIR="/home/swnam/Jenkins/nexus-data"
 
 # Restic 저장소 연결 확인
 if restic -r $RESTIC_REPO_NEXUS snapshots > /dev/null 2>&1; then
