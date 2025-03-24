3dd343288aba874e5bd0ca49b9ccf43ec144784e
swnam <150226255+suwnam@users.noreply.github.com>
2025-03-24 15:36:50 +0900

    Update checklist_nexus.sh

--- checklist_nexus.sh
+++ checklist_nexus.sh
@@ -4,28 +4,32 @@
 ## version: v0.1
 ## date: 2025-03-24
 
+RESTIC_REPO_NEXUS=$RESTIC_REPO/test_nexus
+BACKUP_DIR="/home/popcornsar/DevOps/03_Nexus/nexus-data"
+
 # 스냅샷의 tag 가져오기
 BACKUP_DATE=$(date +%y%m%d)
 
-tags=$(restic -r "$RESTIC_REPO_NEXUS" snapshots latest --json | jq -r '.[-1].tags[]')
+snapshot_id=$(restic -r "$RESTIC_REPO_NEXUS" snapshots latest --json | jq -r '.[-1].short_id')
+tags=$(restic -r "$RESTIC_REPO_NEXUS" snapshots $snapshot_id --json | jq -r '.[-1].tags[]')
 
 # 오늘자 스냅샷 여부 확인
-if echo "$tags" | grep -q "^$BACKUP_DATE$"; then
-  echo "(+) found today snapshots."
+if [[ "$tags" == *"$BACKUP_DATE"* ]]; then
+  echo "[+] found today snapshots."
 else
-  echo "(-) No today snapshots found. exit..."
+  echo "[-] No today snapshots found. exit..."
   exit 1
 fi
 
-echo "(+) Target snapshot ID: $snapshot_id"
+echo "[*] Target snapshot ID: $snapshot_id"
 echo "==========================="
 
 # Primary Nexus file lists
 nexus_paths=(
-  "$BACKUP_DIR/nexus-data/blobs"
-  "$BACKUP_DIR/nexus-data/db"
-  "$BACKUP_DIR/nexus-data/etc"
-  "$BACKUP_DIR/nexus-data/orient"
+  "$BACKUP_DIR/blobs"
+  "$BACKUP_DIR/db"
+  "$BACKUP_DIR/etc"
+  "$BACKUP_DIR/orient"
 )
 
 # Check latest snapshots have primary files
@@ -34,16 +38,15 @@ check_paths() {
   shift
   local paths=("$@")
 
-  echo "[+] Checking if exists $label..."
+  echo "[*] Checking if exists $label..."
   for path in "${paths[@]}"; do
     if restic -r $RESTIC_REPO_NEXUS ls "$snapshot_id" | grep -q "$path"; then
-      echo "(+) SUCCESS : $path"
+      echo "[+] SUCCESS : $path"
     else
-      echo "(-) FAIL : $path"
+      echo "[-] FAIL : $path"
     fi
   done
   echo
 }
 
-check_paths "Nexus" "${nexus_paths[@]}"
-
+check_paths "Nexus_primary_files" "${nexus_paths[@]}"
